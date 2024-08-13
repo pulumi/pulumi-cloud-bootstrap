0a604b8d0adebc44de30383ba4eaf4577778907e
Luke Hoban <lukehoban@gmail.com>
2024-08-13 13:17:07 -0700

    Support for base envs

--- bootstrap/index.ts
+++ bootstrap/index.ts
@@ -1,9 +1,26 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as pulumiservice from "@pulumi/pulumiservice";
+import * as command from "@pulumi/command";
+import remoteOriginUrl from "git-remote-origin-url";
 
 const boostrapProject = pulumi.getProject();
 const bootstrapStack = pulumi.getStack();
 const orgName = pulumi.getOrganization();
+
+const config = new pulumi.Config();
+const baseProject = config.get("baseProject") || "base-yaml";
+const baseProjectPath = `projects/${baseProject}`;
+
+// Map the selected project to the Pulumi project name.
+// TODO: We could read this from the filesystem instead of hadcoding it here.
+const projectName = {
+    "base-yaml": "base",
+    "service": "service",
+    "base": "base",
+}[baseProject];
+if (!projectName) {
+    throw new Error(`The project ${baseProject} does not exist in this repository`)
+}
 
 const devAccessTeam = new pulumiservice.Team("dev", {
     organizationName: orgName,
@@ -37,122 +54,88 @@ new pulumiservice.TeamStackPermission("boostrap-prod", {
     organization: orgName,
     project: boostrapProject,
     stack: bootstrapStack,
-    team: devAccessTeam.name.apply(name => name!),
+    team: productionAccessTeam.name.apply(name => name!),
     permission: pulumiservice.TeamStackPermissionScope.Admin,
 });
 
 // TODO:
 // * Set up templates (environment, app)
-// * Create two instances of the environment (dev, prod)
-// * Create ESC environments 
+
+export let baseStacks: Record<string, pulumi.Output<string>> = {};
 
 for (const env of ["dev" , "prod" ]) {
     const baseStack = new pulumiservice.Stack(`base-${env}`, {
         organizationName: orgName,
-        projectName: "base",
+        projectName: projectName,
         stackName: env,
     });
 
     const baseStackTag = new pulumiservice.StackTag(`base-${env}`, {
         organization: orgName,
-        project: "base",
+        project: baseStack.projectName,
         stack: baseStack.stackName,
         name: "environment",
         value: env,
-    })
+    });
+
+    const github = remoteOriginUrl().then(url => {
+        if (!url) {
+            throw new Error("Bootstrap project requires being run within a Git repository with a remote origin.")
+        }
+        const regexp = /https:\/\/github\.com\/([^\/]+)\/([^\/]+)\.git/;
+        const match = url.match(regexp);
+        if (!match) {
+            return {};
+        }
+        return {
+            repository: `${match[1]}/${match[2]}`,
+            paths: [baseProjectPath],
+            deployCommits: true,
+            previewPullRequests: true,
+        };
+    });
     
     const baseStackDeploymentSetting = new pulumiservice.DeploymentSettings(`base-${env}`, {
         organization: orgName,
-        project: "base",
+        project: baseStack.projectName,
         stack: baseStack.stackName,
         sourceContext: {
             git: {
-                repoDir: "projects/base-yaml",
+                repoDir: baseProjectPath,
                 // TODO: Should this use different branches for deploying to dev and prod environments?
                 branch: "main", 
             },
         },
-        github: {
-            repository: "pulumi-contoso/pulumi-cloud-bootstrap",
-            paths: ["projects/base-yaml"],
-            deployCommits: true,
-            previewPullRequests: true,
-        },
+        github: github,
     });
-    // TODO: Actually trigger the deployment?  Right now, the user needs to go to the page and click "Actions -> Update"
 
-//     const baseESCEnv = new pulumiservice.Environment(`base-${env}`, {
-//         organization: orgName,
-//         name: `base/${env}`,
-//         yaml: new pulumi.asset.StringAsset(`\
-//             values:
-//                 base:
-//                     fn::open::pulumi-stacks:
-//                         stacks:
-//                             base:
-//                                 stack: base/${baseStack.stackName}
-//                 pulumiConfig:
-//                     baseEnvironmentName: \${base.stacks.base.environmentName}
-// `)
-//     });
+    // Trigger a deployment when the stack or deployment settings change
+    // TODO: Should this be part of the `Stack` or `DeploymentSetting` resource to trigger this 
+    // automatically on updates?
+    const deployment = new command.local.Command(`deploy-base-${env}`, {       
+        create: pulumi.interpolate`pulumi deployment run update --stack ${orgName}/${baseStack.projectName}/${baseStack.stackName}`,
+        dir: baseStackDeploymentSetting.sourceContext.apply(source => `../${source.git!.repoDir}`),
+        triggers: [baseStackDeploymentSetting.id, baseStack.id],
+    });
+
+    const envYaml = baseStack.stackName.apply(stackName => new pulumi.asset.StringAsset(`\
+        values:
+            stack:
+                fn::open::pulumi-stacks:
+                    stacks:
+                        base:
+                            stack: base/${stackName}
+            pulumiConfig:
+                baseEnvironmentName: \${stack.base.environmentName}`));
+
+    const baseESCEnv = new pulumiservice.Environment(`base-${env}`, {
+        organization: orgName,
+        name: `base-${env}`,
+        yaml: envYaml,
+    });
+    baseStacks[env] = pulumi.interpolate`https://app.pulumi.com/${orgName}/${baseStack.projectName}/${baseStack.stackName}`;
 }
 
 export const productionAccessToken = prodAccessToken.value;
 
-
-
-// if(awsProductionRole) {
-//     const productionEnv = new pulumiservice.Environment("aws-production", {
-//         organization: orgName,
-//         name: "aws-production",
-//         yaml: new pulumi.asset.StringAsset(`\
-//     values:
-//         aws:
-//             login:
-//                 fn::open::aws-login:
-//                     oidc:
-//                         duration: 1h
-//                         roleArn: ${awsProductionRole}
-//                         sessionName: pulumi-environments-session
-//                         subjectAttributes:
-//                             - currentEnvironment.name
-//                             - pulumi.user.login
-//         environmentVariables:
-//             AWS_ACCESS_KEY_ID: \${aws.login.accessKeyId}
-//             AWS_SECRET_ACCESS_KEY: \${aws.login.secretAccessKey}
-//             AWS_SESSION_TOKEN: \${aws.login.sessionToken}`)
-//     });
-//     const productionEnvPermission = new pulumiservice.TeamEnvironmentPermission("aws-production", {
-//         organization: orgName,
-//         environment: productionEnv.id,
-//         permission: pulumiservice.EnvironmentPermission.Admin,
-//         team: productionAccessTeam.id,
-//     });
-// }
-
-// // for (const env of ["dev", "stage", "prod"]) {
-// //     const stack = new pulumiservice.Stack("networking-production", {
-// //         project: "networking",
-// //         stack: "production",
-// //         config: {
-    
-// //         },
-// //     });
-
-
-// // const settings = new pulumiservice.DeploymentSettings("production", {
-// //     organization: orgName,
-// //     project: stack.projec,
-// //     stack: stack.stack,
-// //     sourceContext: {
-// //         git: {
-// //             repoUrl: "https://github.com/demo/networking"
-// //         },
-// //     },
-// //     github: {
-
-// //     }
-// // })
-
-// // }
 
